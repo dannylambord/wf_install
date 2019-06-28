92d8b4e71b1377147bc61d37d9d9372f2e3b3e95
Ubuntu <dlam@vm2.2zhmvyhjbq4ulcdkeafdzrgrte.zx.internal.cloudapp.net>
2019-06-28 10:05:43 +0000

    Wildfly working script

--- wf.sh
+++ wf.sh
@@ -7,12 +7,12 @@ sudo usermod --shell /bin/bash wf
 
 echo >> wf.service
 echo "[Unit]" >> wf.service
-echo'Description=wf' >> wf.service
+echo 'Description=wf' >> wf.service
 echo  >> wf.service
 echo "[Service]" >> wf.service
 echo 'User=wf' >> wf.service
 echo 'WorkingDirectory=/home/wf' >> wf.service
-echo 'ExecStart=/home/wildfly-10.1.0.Final/bin/standalone.sh' >> wf.service
+echo 'ExecStart=/home/wildfly-10.1.0.Final/bin/standalone -b=0.0.0.0' >> wf.service
 echo  >> wf.service
 echo "[Install]" >> wf.service
 echo 'WantedBy=multi-user.target' >> wf.service
@@ -20,7 +20,7 @@ echo 'WantedBy=multi-user.target' >> wf.service
 sudo mv wf.service /etc/systemd/system/wf.service
 
 sudo su - wf -c "wget https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.tar.gz"
-
+sudo su - wf -c "tar xvf ./wildfly-10.1.0.Final.tar.gz"
 
 sudo systemctl daemon-reload
 
